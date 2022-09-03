b8ef1e692dd4b6c35414b63d1ddd75fea6695274
Talita Miguel <talita.leonardo@gmail.com>
2022-09-02 21:21:21 -0300

    3° - changing index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import { app } from "./app";
 import connectionTest from "./endpoints/connectionTest";
+import { deleteUser } from "./endpoints/deleteUser";
+import { getAllUser } from "./endpoints/getAllUser";
+import { getUserById } from "./endpoints/getUserById";
+import { postLogin } from "./endpoints/postLogin";
+import { postUser } from "./endpoints/postUser";
+import { putUser } from "./endpoints/putUser";
+
 
 app.get("/connectionTest", connectionTest)
+app.get("/users", getAllUser)
+app.get("/users/byId/:id", getUserById)
+
+app.post("/users", postUser)
+app.post("/users/login", postLogin)
+
+app.put("/users/:id", putUser)
+
+app.delete("/users/:id", deleteUser)
